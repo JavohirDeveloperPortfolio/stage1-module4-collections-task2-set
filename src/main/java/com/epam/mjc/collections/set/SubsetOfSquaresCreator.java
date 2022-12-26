8f9ae721a9837d06f4aa2dde2fa7d4fe4b0db69a
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-12-26 11:25:33 +0500

    done!

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> set = new TreeSet<>();
+        for (Integer integer : sourceList) {
+            int power = integer*integer;
+            if ((power >= lowerBound) && (power <= upperBound)){
+                set.add(power);
+            }
+        }
+        return set;
     }
 }
